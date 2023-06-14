f5a30b1cbb5587cfb5ea7edf96fe9040a07768b7
Tsega Tigneh <136186978+segish@users.noreply.github.com>
2023-06-14 18:49:34 +0300

    Update axios.js

--- social/src/axios.js
+++ social/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-    baseURL: "https://tsegish.onrender.com/api/",
+    baseURL: "https://segish-social.onrender.com/api/",
     withCredentials: true,
 })
